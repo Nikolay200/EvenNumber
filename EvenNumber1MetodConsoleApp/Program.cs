b566a261527f371a59d1664b362aeea627aac9e8
Nikolay200 <kolekdoma@mail.ru>
2021-05-25 10:05:24 +0300

    1 Метод определения чётности и нечётности числа

---
+++ EvenNumber1MetodConsoleApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace EvenNumberConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            Console.WriteLine("Введите число: ");
+            var input = Console.ReadLine();
+
+            var userInput = int.TryParse(input, out int rightInput);
+
+            while (userInput == false)
+            {
+                Console.WriteLine("Вы ввели неподходящий символ.");
+                goto Start;
+            }
+
+            while (userInput == true)
+            {
+                if (rightInput != 0)
+                {
+                    if (IsEven(rightInput) == true)
+                    {
+                        Console.WriteLine("Ваше число является чётным.");
+
+
+                    }
+                    if (IsEven(rightInput) == false)
+                    {
+                        Console.WriteLine("Ваше число является нечётным.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Скорее всего вы ввели 0. Введите другое значение.");
+                    goto Start;
+                }
+                Console.WriteLine("Хотите проверить другое число? Введите Да или Нет");
+                var answer = Console.ReadLine();
+                if (answer.ToLower() == "да")
+                {
+                    goto Start;
+                }
+                else
+                {
+                    Console.WriteLine("Хотите выйти? Введите Да или Нет");
+                    var roundAnswer = Console.ReadLine();
+                    if (roundAnswer.ToLower() == "нет")
+                    {
+                        goto Start;
+                    }
+                    else
+                    {
+                        Console.WriteLine("До свидания!");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static bool IsEven(int rightInput)
+        {
+            return (rightInput & 1) == 0;
+        }
+    }
+}
